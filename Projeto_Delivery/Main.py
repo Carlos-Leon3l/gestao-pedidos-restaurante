48f7f5249900a8053d3ad1b79fcd2d59c24c2b64
Carlos Leon3l <carlos.leonel268@gmail.com>
2025-09-03 11:34:36 -0300

    modified main.py

--- Projeto_Delivery/Main.py
+++ Projeto_Delivery/Main.py
@@ -23,12 +23,20 @@ class Restaurante:
     def adicionar_item_cardapio(self, item_para_aducionar):
         self.cardapio.append(item_para_aducionar)
         
-
+    def getNome(self):
+        return self.nome
+        
 class ItemCardapio:
     def __init__(self, nome_item:str, descricao:str, preco:int):
         self.nome_item = nome_item
         self.descricao = descricao
         self.preco = preco
+        
+    def getNome(self):
+        return self.nome_item 
+    
+    def getDescricao(self):
+        return self.descricao
     
     def getPreco(self):
         return self.preco
@@ -51,7 +59,7 @@ class Pedido:
     
     def definir_pagamento(self, objeto_pagamento):
         self.info_pagamento = objeto_pagamento
-        pass
+        return self.info_pagamento
         
     def calcular_valor_total_pedido(self):
         soma_itens = 0
@@ -61,25 +69,25 @@ class Pedido:
             soma = preco
             soma_itens = soma_itens + soma
         self.valor_total = soma_itens
-        return self.valor_total
+        self.detalhes_entrega = self.valor_total + self.detalhes_entrega
+        return self.detalhes_entrega
    
-        # if(self.info_pagamento.escolher_forma_pagamento()):
-        #     for item in self.lista_itens:
-        #         preco_item = item.getPreco()
-        #         valor_total_itens =+ preco_item
-        #     valor_total_itens + self.detalhes_entrega.getTaxa()
-        # self.valor_total = valor_total_itens
-        # return self.valor_total
- 
     def status_do_pedido_atual(self):
         self.status_entrega = "A CAMINHO"
         
-    def definir_entrega(self, objeto_entrega):
-        self.definir_entrega = objeto_entrega
-        pass
+    def valor_taxa_produto(self, opcao_de_entrega_escolhida):
+        self.detalhes_entrega = opcao_de_entrega_escolhida
         
+    def recibo_de_entrega(self):
+        print(f"NOME DO CLIENTE: {self.nome_cliente_pedido}")
+        print(f"NOME DO PEDIDO: {self.restaurante.getNome()}")
+        print(f"LISTA DE PEDIDO:")
+        for itens in self.lista_itens:
+             print(f"[{itens.getNome()}, {itens.getDescricao()},{itens.getPreco()}] \n")
+        print(f"VALOR TOTAL DO PEDIDO: {self.detalhes_entrega}")
+    
 class Pagamento:
-    def __init__(self, metodo_pagamento):
+    def __init__(self, metodo_pagamento:str):
         self.metodo_pagamento = metodo_pagamento
         self.status_pagamento = "PENDENTE"
     
@@ -93,13 +101,16 @@ class Pagamento:
             print("Pagamento esta sendo processado ....")
             sleep(5)
             print("Seu pagamento foi efetuado, compra a caminho")
+            return self.metodo_pagamento
             
         elif(self.metodo_pagamento == "PIX".upper()):
             print("Seu pagamento foi efetuado, compra a caminho")
-        
+            return self.metodo_pagamento
+
         elif(self.metodo_pagamento == "Dinheiro".upper()):
             print("Pague na hora do pedido ser entregue a sua casa")
-             
+            return self.metodo_pagamento
+
             
 class Entrega:
     def __init__(self, metodo_entrega):
@@ -116,15 +127,24 @@ class Entrega:
             self.taxa_entrega = 5
             
     def getTaxa(self):
-    
         return self.taxa_entrega 
 
     
-h = Pedido(Cliente("c","madre",24124),Restaurante("pizza","italiana"))
+pedido_cliente = Pedido(Cliente("carlos","madre",24124),Restaurante("pizzaria","italiana"))
 
-h.adicionar_item(ItemCardapio("pizza","pizzalegal",20))
-h.adicionar_item(ItemCardapio("pizza","pizzalegal",50))
+pedido_cliente.adicionar_item(ItemCardapio("pizza peperroni","boa pizza",20))
 
+pedido_cliente.adicionar_item(ItemCardapio("pizza de mussarela","essa e tbm uma boa pizza",50))
 
-print(h.calcular_valor_total_pedido())
+entrega = Entrega("RAPIDO")
+
+pagamento = Pagamento("PIX")
+
+print(pagamento.escolher_forma_pagamento())
+
+pedido_cliente.valor_taxa_produto(entrega.getTaxa())
+
+print(pedido_cliente.calcular_valor_total_pedido())
+
+print(pedido_cliente.recibo_de_entrega())
  
